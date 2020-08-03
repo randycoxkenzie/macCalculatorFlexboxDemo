60365e4586b7914014c66bd890c5f9c368358411
Randy Cox <randy@kenzie.academy>
2020-08-03 17:08:10 -0400

    Pretty well finished HTML and CSS.

---
+++ README.md
@@ -0,0 +1,13 @@
+Parts of a tree
+
+* Nodes
+  * root node
+  * parent node
+  * child node
+  * leaf node
+* Edge
+
+API
+Application Programming Interface
+
+***
---
+++ script.js
@@ -0,0 +1,7 @@
+let opnode = document.getElementById('operators')
+let opnode = document.querySelector('#operators')
+
+document.getElementsByClassName()
+
+document.querySelector('#operators').appendChild(newbtn)
+document.querySelector('#digits button:nth-child(2)')
